3025ffc442331694a2428192891ecf81f1feb585
0101spbl <114441053+0101spbl@users.noreply.github.com>
2022-12-06 17:08:22 +0100

    update order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,24 @@ class OrdersController < ApplicationController
   
 
     def create
+        # Before the rescue, at the beginning of the method
+@stripe_amount = 500
+begin
+  customer = Stripe::User.create({
+  email: params[:stripeEmail],
+  source: params[:stripeToken],
+  })
+  charge = Stripe::Order.create({
+  user: user.id,
+  amount: @stripe_amount,
+  description: "Achat d'un article",
+  currency: 'eur',
+  })
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to new_order_path
+end
+# After the rescue, if the payment succeeded
   
     end
   end
