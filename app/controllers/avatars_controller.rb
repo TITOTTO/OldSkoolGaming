1fbc2e94b351ba96d1ea08f2c099f3be5b4ac685
TITOTTO <kuaker26@gmail.com>
2022-12-07 22:21:41 +0100

    Fonctionne avec Flyio

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class AvatarsController < ApplicationController
     def create
         @user = User.find(params[:user_id])
         @user.avatar.attach(params[:avatar])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-    def prix_total
-        @total = 0
-        current_user.cart.articles.each do |prix|
-            @total = @total + prix.price
-        end
-        return @total
-    end
 end
