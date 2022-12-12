348bf61da89f37a28f14f552458055e4c59ccb07
TITOTTO <kuaker26@gmail.com>
2022-12-12 08:43:12 +0100

    comment Ok

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
+    @article = Article.find(params[:article_id])
     Comment.create(title: params[:title], content: params[:content], user: current_user, commentable_id: params[:commentable_id], commentable_type: params[:commentable_type])
+    redirect_to article_path(@article)
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
