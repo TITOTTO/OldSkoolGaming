f55d2d9580b6d0c4eccbab423e16d229aa917fd6
widmaker <widmaker666@msn.com>
2022-12-15 13:57:36 +0100

    Validates de la table article fait, par contre impossible de le faire pour les commentaire car je ne sais pas quel est la table utilisé et de tout façon y'a ça bug qd j'essaye avec une des deux

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
-class Comment < ApplicationRecord
+class Comment < ApplicationRecord 
+
     belongs_to :user
     belongs_to :commentable, polymorphic: true
     has_many :comments, as: :commentable
